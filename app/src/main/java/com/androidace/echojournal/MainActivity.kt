7cd26332390835942abcfde4ac2607f8c67d4fdb
Aseem Sharma <er.sharmaaseem@gmail.com>
2025-01-02 13:57:15 +0530

    Initial commit

---
+++ app/src/main/java/com/androidace/echojournal/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.androidace.echojournal
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.androidace.echojournal.ui.theme.EchoJournalTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            EchoJournalTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
